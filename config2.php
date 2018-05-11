edf9cabc8a04b3d29ffdefa3715b3c8ec8e29a3f
xxkasperxx <leviwurtz2622@yahoo.com>
2018-05-11 17:04:42 +0000

    Delete config2.php

--- config2.php
+++
@@ -1,7 +0,0 @@
-<?php
-mysql_connect('mysql11.000webhost.com','a7598469_levi','maxgaming88') or die('could not connect to MYSQLs' . mysql_error());
-
-mysql_select_db("a7598469_golf") or die ("Couldnt find table Database. Sorry");
-
-
-?>
